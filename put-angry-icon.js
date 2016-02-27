ff62c61f69006c4ef6fd462b71697796c57901f9
Ku Lok Sun <sundayku199911@yahoo.com.hk>
2016-02-27 16:20:11 +0800

    Include jQuery

--- put-angry-icon.js
+++ put-angry-icon.js
@@ -14,7 +14,7 @@ var scroll_to_bottom_feed = function() {
 }
 
 var show_emotion_bar = function() {
-    var emontion_bar_parent = $$('.uiContextualLayerParent ._khz > div');
+    var emontion_bar_parent = $('.uiContextualLayerParent ._khz > div');
     for (var i = 0; i < emontion_bar_parent.length; ++i) {
         emontion_bar_parent[i].className = "_1oxj _10ir";
     }
@@ -36,12 +36,12 @@ var click_btn_on_all_post_one_by_one = function(icons) {
 }
 
 var put_angry_face_on_all_post = function() {
-    var angry_icons = $$('._39m[data-reaction="8"]');
+    var angry_icons = $('._39m[data-reaction="8"]');
     click_btn_on_all_post_one_by_one(angry_icons);
 }
 
 var cancel_all_emotion_on_post = function(){
-    var cancel_btns = $$('a.UFILikeLink.UFILinkBright');
+    var cancel_btns = $('a.UFILikeLink.UFILinkBright');
     click_btn_on_all_post_one_by_one(cancel_btns);
 }
 
@@ -57,4 +57,4 @@ chrome.runtime.onMessage.addListener(
         } else if (request.action == "cancel") {
             cancel_all_emotion_on_post();
         }
-});;
+});
